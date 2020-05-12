d28587913c209202aedce4a6906ae436f47b6719
MajorVictory <MajorVictory@github.com>
2020-05-12 12:17:22 -1000

    fix for triumph and despair not generating extra success and failure for swrpg dice

--- includes/login.js
+++ includes/login.js
@@ -702,6 +702,9 @@ function login_initialize(container) {
                         despair += (currentlabel.split('y').length - 1);
                         dark += (currentlabel.split('z').length - 1);
                         light += (currentlabel.split('Z').length - 1);
+
+                        success += triumph;
+                        failure += despair;
                     }
 
                     rolls += 's'.repeat(success);
